25b68309228fc0bbf5660c98790b03b4ffb8e2cc
CucsiDarz <63427244+Cucsidarz@users.noreply.github.com>
2020-12-22 23:48:00 +0000

    Update javascript.js

--- Breakout game/javascript.js
+++ Breakout game/javascript.js
@@ -10,6 +10,30 @@ var paddleHeight = 10;
 var paddleWidth = 75;
 var paddleX = (canvas.width-paddleWidth) / 2;
 
+var rightPressed = false;
+var leftPressed = false;
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e){
+    if(e.key =="Right" || e.key == "ArrowRight"){
+      rightPressed = true; 
+    }
+    else if(e.key == "Left" || e.key =="ArrowLeft"){
+
+    }
+}
+
+function keyUpHandler(e){
+     if(e.key == "Right" || e.key == "ArrowRight"){
+         rightPressed = false;
+     }
+     else if(e.key == "Left" || e.key =="ArrowLeft"){
+         leftPressed =false;
+     }
+}
+
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI*2);
@@ -42,6 +66,26 @@ if( x + dx > canvas.width-ballRadius || x + dx < 0){
 
 if( y + dy > canvas.height-ballRadius || y + dy <0){
     dy = -dy;
+
+    if(rightPressed){
+        paddleX +=7;
+        if(paddleX + paddleWidth > canvas.width){
+            paddleX = canvas.width - paddleWidth;
+        }
+    }
+
+    else if(leftPressed){
+        paddleX -= 7;
+        if(paddleX <0){
+            paddleX = 0;
+        }
+    }
+    x += dx;
+    y += dy;
 }
 }
+
+
+
+
 setInterval(draw, 10);
