774c4b69cf9b377393c79f0a7c80239ab17a40b7
CucsiDarz <63427244+Cucsidarz@users.noreply.github.com>
2020-12-23 12:55:40 +0000

    clear up of javascript, added paddle moverment

--- Breakout game/javascript.js
+++ Breakout game/javascript.js
@@ -1,37 +1,35 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+var ballRadius = 10;
 var x = canvas.width/2;
 var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
-var ballRadius = 10;
-
 var paddleHeight = 10;
 var paddleWidth = 75;
-var paddleX = (canvas.width-paddleWidth) / 2;
-
+var paddleX = (canvas.width-paddleWidth)/2;
 var rightPressed = false;
 var leftPressed = false;
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
-function keyDownHandler(e){
-    if(e.key =="Right" || e.key == "ArrowRight"){
-      rightPressed = true; 
+function keyDownHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = true;
     }
-    else if(e.key == "Left" || e.key =="ArrowLeft"){
-
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = true;
     }
 }
 
-function keyUpHandler(e){
-     if(e.key == "Right" || e.key == "ArrowRight"){
-         rightPressed = false;
-     }
-     else if(e.key == "Left" || e.key =="ArrowLeft"){
-         leftPressed =false;
-     }
+function keyUpHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = false;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = false;
+    }
 }
 
 function drawBall() {
@@ -41,7 +39,6 @@ function drawBall() {
     ctx.fill();
     ctx.closePath();
 }
-
 function drawPaddle() {
     ctx.beginPath();
     ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
@@ -53,39 +50,34 @@ function drawPaddle() {
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
-    x += dx;
-    y += dy;
     drawPaddle();
-if ( y + dy > canvas.height || y + dy < 0){
-    dy = -dy;
-}
-
-if( x + dx > canvas.width-ballRadius || x + dx < 0){
-    dx = -dx;
-}
-
-if( y + dy > canvas.height-ballRadius || y + dy <0){
-    dy = -dy;
-
-    if(rightPressed){
-        paddleX +=7;
-        if(paddleX + paddleWidth > canvas.width){
-            paddleX = canvas.width - paddleWidth;
+    
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if(y + dy < ballRadius) {
+        dy = -dy;
+    }
+    else if(y + dy > canvas.height-ballRadius) {
+        if(x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
+        }
+        else {
+            alert("GAME OVER");
+            document.location.reload();
+            clearInterval(interval);
         }
     }
-
-    else if(leftPressed){
+    
+    if(rightPressed && paddleX < canvas.width-paddleWidth) {
+        paddleX += 7;
+    }
+    else if(leftPressed && paddleX > 0) {
         paddleX -= 7;
-        if(paddleX <0){
-            paddleX = 0;
-        }
     }
+    
     x += dx;
     y += dy;
 }
-}
 
-
-
-
-setInterval(draw, 10);
+var interval = setInterval(draw, 10);
